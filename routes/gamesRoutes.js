b83be888041b6428c2428e676c0fb2f48c516784
Max Peiros <mpeiros@gmail.com>
2017-09-25 17:30:44 -0700

    Update top games route to get 30 games

--- routes/gamesRoutes.js
+++ routes/gamesRoutes.js
@@ -3,7 +3,7 @@ const keys = require('../config/keys');
 
 module.exports = app => {
   app.get('/api/games/top', async (req, res) => {
-    const games = await axios.get(`${keys.twitchApiUrl}/games/top?client_id=${keys.twitchClientID}`);
+    const games = await axios.get(`${keys.twitchApiUrl}/games/top?limit=30&client_id=${keys.twitchClientID}`);
 
     res.send(games.data);
   });
