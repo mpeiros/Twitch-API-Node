7c88f3984aaa39ec83e6c91fd306b0f458a19a74
Max Peiros <mpeiros@gmail.com>
2017-09-25 17:41:43 -0700

    Add all streams route

--- routes/streamsRoutes.js
+++ routes/streamsRoutes.js
@@ -2,5 +2,9 @@ const axios = require('axios');
 const keys = require('../config/keys');
 
 module.exports = app => {
-  
+  app.get('/api/streams', async (req, res) => {
+    const streams = await axios.get(`${keys.twitchApiUrl}/streams?client_id=${keys.twitchClientID}`);
+
+    res.send(streams.data);
+  });
 };
